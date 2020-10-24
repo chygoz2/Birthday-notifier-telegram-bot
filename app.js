4aad098f80bd49fcc30f5afae793bb2dbe3a0538
Chigozie <chygoz2@gmail.com>
2020-10-24 20:50:49 +0100

    Add customizable port

--- app.js
+++ app.js
@@ -68,5 +68,9 @@ cron.schedule(process.env.CRON_STRING, () => {
   })
 })
 
+const port = process.env.PORT || 8080
+
 bot.launch();
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}`)
+});

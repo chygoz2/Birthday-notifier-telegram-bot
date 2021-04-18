d879682a8505fb7cd79307d273da4affdc80101d
Chigozie <chygoz2@gmail.com>
2021-04-18 17:11:19 +0100

    Fix connection failure to postgres

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,6 +1,16 @@
 const { Sequelize } = require('sequelize');
-const pgConnectionString = process.env.PG_CONNECTION_STRING;
 
-const sequelize = new Sequelize(pgConnectionString);
+// const sequelize = new Sequelize(process.env.PG_CONNECTION_STRING);
+
+const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USERNAME, process.env.PG_PASSWORD, {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
 
 module.exports = sequelize
